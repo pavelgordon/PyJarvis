820156dd5db282f863b9767d6d973549acbdbaeb
Pavel Gordon <gordon.pav@gmail.com>
2017-05-02 16:31:36 +0300

    Implement login on ororo.tv and bypass cloudflare

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
-token = "YOUR TELEGRAM TOKEN THERE"
+token = "token"
+user = 'username'
+password = 'pass'
+# token = "YOUR TELEGRAM TOKEN THERE"
+
+# optional
+proxies = {
+  'http': 'http://proxy.t-systems.ru:3128',
+  'https': 'http://proxy.t-systems.ru:3128',
+}
+
+ororo_payload = {
+    'user[email]': user,
+    'user[password]': password,
+}
+# Need this cause of
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Platform; Security; OS-or-CPU; Localization; rv:1.4) Gecko/20030624 Netscape/7.1 (ax)'
+}
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+import requests
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     print(message.text)
-    bot.send_message(message.chat.id, message.text)
 
+    bot.send_message(message.chat.id, message.text)
+    series = get_new_series()
+    bot.send_message(message.chat.id, series)
+
+
+def get_new_series():
+    # Use 'with' to ensure the session context is closed after use.
+
+    with requests.Session() as s:
+        # Sign in ororo.tv to be able to see all series
+        json = 'https://ororo.tv/en/users/sign_in.json'
+        p = s.post(json, data=config.ororo_payload, proxies=config.proxies, headers=config.headers)
+        # TODO check if status is 200
+        print(p.text)
+
+        # Load series page
+        r = s.get('https://ororo.tv/en/shows/gotham',  proxies=config.proxies, headers=config.headers)
+        print( r.text)
+        return r.text
+        #     # etc...
 if __name__ == '__main__':
      bot.polling(none_stop=True)
