99e2f9b6b794a014d4b5689631834c422136ae4d
Pavel Gordon <gordon.pav@gmail.com>
2017-05-04 17:10:01 +0300

    Implement simple polling system for ororo.tv
    User now can
     - subscribe to updates
     - unsubscribe to updates
     - check updates manually
    
    Update is a message with new episode of my(for now) series.
    Updates will come every 5 minutes

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
-import requests
-
-bot = telebot.TeleBot(config.token)
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    print(message.text)
-
-    bot.send_message(message.chat.id, message.text)
-    series = get_new_series()
-    bot.send_message(message.chat.id, series)
+import lib
+import time
+import logging
+# TODO rewrite using threading as better concurrency lib in python3
+import _thread
 
 
-def get_new_series():
-    # Use 'with' to ensure the session context is closed after use.
+bot = telebot.AsyncTeleBot(config.token)
+series_subscribers = {}
+TIMEOUT = 60  * 5 # 5 minutes
 
-    with requests.Session() as s:
-        # Sign in ororo.tv to be able to see all series
-        json = 'https://ororo.tv/en/users/sign_in.json'
-        p = s.post(json, data=config.ororo_payload, proxies=config.proxies, headers=config.headers)
-        # TODO check if status is 200
-        print(p.text)
 
-        # Load series page
-        r = s.get('https://ororo.tv/en/shows/gotham',  proxies=config.proxies, headers=config.headers)
-        print( r.text)
-        return r.text
-        #     # etc...
+@bot.message_handler(commands=['check_series'])
+def check_new_series(message):
+    global previous_series
+    series = lib.get_new_series()
+    if previous_series != series:
+        bot.send_message(message.chat.id, lib.format_message(series))
+        previous_series = series
+
+
+@bot.message_handler(commands=['subscribe'])
+def subscribe(message):
+    series_subscribers[message.chat.id] = {"previous_series": []}
+    bot.send_message(message.chat.id, "subscribed on series updates")
+
+
+@bot.message_handler(commands=['unsubscribe'])
+def unsubscribe(message):
+    series_subscribers.pop(message.chat.id)
+    bot.send_message(message.chat.id, "unsubscribed on series updates")
+
+
+def broadcast_new_series():
+    global previous_series
+    while True:
+        logging.info("[SERIES] Started new broadcasting")
+        logging.info("[SERIES] Subscribers {0}".format(series_subscribers))
+        series = lib.get_new_series()
+        for user in series_subscribers.items():
+            previous_series = user[1]["previous_series"]
+            if previous_series != series:
+                bot.send_message(user[0], lib.format_message(series))
+                user[1]["previous_series"] = series
+        time.sleep(TIMEOUT)
+
+
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    previous_series = []
+    logging.getLogger('requests').setLevel(logging.CRITICAL)
+    logging.basicConfig(format='[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s', level=logging.INFO,
+                        # filename='bot_log.log',
+                         datefmt='%d.%m.%Y %H:%M:%S')
+    logging.info("[App] Logger init")
+    _thread.start_new_thread(broadcast_new_series, ())
+    logging.info("[App] Bot start")
+    bot.polling(none_stop=True)
+
